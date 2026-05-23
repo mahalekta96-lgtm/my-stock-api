4fd7577872d3d66a9b6bde1bcb0914a111054028
mahalekta96-lgtm <mahalekta96@gmail.com>
2026-05-23 18:21:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -68,5 +68,3 @@ def health():
 def home():
     return jsonify({"message": "Your Personal Stock Analysis API"})
 
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=False, host="0.0.0.0", port=port)
